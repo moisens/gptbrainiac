48d8d2c889517a7d04aecc649fcb78e677f4cea6
Moise <moise_ns@outlook.com>
2024-07-03 17:04:29 +0200

    navlink functionality + themetoggle component setup

--- app/components/SidebarHeader.jsx
+++ app/components/SidebarHeader.jsx
@@ -1,5 +1,15 @@
+import { SiOpenaigym } from "react-icons/si";
+import { TbCircuitChangeover } from "react-icons/tb";
+import ThemeToggle from "./ThemeToggle";
+
 const SidebarHeader = () => {
-  return <div>sidebar header</div>;
+  return (
+    <div className="flex items-center mb-4 gap-4 px-4">
+      <TbCircuitChangeover className="w-10 h-10 text-primary rotate-90" />
+      <h2 className="text-xl font-extrabold text-primary">brainiac</h2>
+      <ThemeToggle />
+    </div>
+  );
 };
 
 export default SidebarHeader;
---
+++ app/utils/utils.navlink.js
@@ -0,0 +1,8 @@
+const links = [
+  { href: "/chat", label: "chat" },
+  { href: "/tours", label: "tours" },
+  { href: "/tours/new-tour", label: "new tour" },
+  { href: "/profile", label: "profile" },
+];
+
+export default links;
