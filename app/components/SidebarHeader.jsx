ddd51b1b34f06c50a960ccff106dda7511762515
Moise <moise_ns@outlook.com>
2024-07-04 16:52:50 +0200

    theme

--- app/components/SidebarHeader.jsx
+++ app/components/SidebarHeader.jsx
@@ -6,7 +6,7 @@ const SidebarHeader = () => {
   return (
     <div className="flex items-center mb-4 gap-4 px-4">
       <TbCircuitChangeover className="w-10 h-10 text-primary rotate-90" />
-      <h2 className="text-xl font-extrabold text-primary">brainiac</h2>
+      <h2 className="text-xl font-extrabold text-primary mr-auto">brainiac</h2>
       <ThemeToggle />
     </div>
   );
