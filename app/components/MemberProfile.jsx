9f94689c18c1c3096fd02c7e0630b4e7f62a6c71
Moise <moise_ns@outlook.com>
2024-07-04 14:26:37 +0200

    member profile user data + mofif home page

--- app/components/MemberProfile.jsx
+++ app/components/MemberProfile.jsx
@@ -1,5 +1,13 @@
-const MemberProfile = () => {
-  return <div>member profile</div>;
+import { currentUser, UserButton, auth } from "@clerk/nextjs";
+
+const MemberProfile = async () => {
+  const user = await currentUser();
+  return (
+    <div className="px-4 flex items-center gap-2">
+      <UserButton afterSignOutUrl="/" />
+      {user.emailAddresses[0].emailAddress}
+    </div>
+  );
 };
 
 export default MemberProfile;
