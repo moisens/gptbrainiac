89cf42399a4f07ac94b5c2803a945112cff0e7ca
Moise <moise_ns@outlook.com>
2024-07-08 11:37:32 +0200

    Image gen with openai

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,18 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "oaidalleapiprodscus.blob.core.windows.net",
+        port: "",
+        pathname: "/private/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
