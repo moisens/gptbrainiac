0c76dbbf59af88a7f3b90ceec3156d222b181c6b
Moise <moise_ns@outlook.com>
2024-07-08 13:56:41 +0200

    removed sign in sign up pages + removed profile nav link

--- app/utils/utils.navlink.js
+++ app/utils/utils.navlink.js
@@ -2,7 +2,7 @@ const links = [
   { href: "/chat", label: "chat" },
   { href: "/tours", label: "tours" },
   { href: "/tours/new-tour", label: "new tour" },
-  { href: "/profile", label: "profile" },
+  // { href: "/profile", label: "profile" },
 ];
 
 export default links;
